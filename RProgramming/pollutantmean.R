7daeafc096b51a334178a6c68e4e2031c16b6b35
gary-dalton <gary@ggis.biz>
2015-05-15 06:57:13 -0500

    Working on pollution data

--- RProgramming/pollutantmean.R
+++ RProgramming/pollutantmean.R
@@ -2,6 +2,16 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     ## 'directory' is a character vector of length 1 indicating
     ## the location of the CSV files
     
+    data = NULL
+    for(i in 1:length(id)){
+        fname <- paste(sprintf("%03d", i), "csv", sep=".")
+        path<- paste(dir, fname, sep="/")
+        data<- rbind(data, read.csv(path))
+        
+    }
+    
+    
+    
     ## 'pollutant' is a character vector of length 1 indicating
     ## the name of the pollutant for which we will calculate the
     ## mean; either "sulfate" or "nitrate".
